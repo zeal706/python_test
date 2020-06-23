317c7e358a4846c728a33bb528d2f7b09d9bfe47
Zeal706 <1615284247@qq.com>
2020-06-23 23:30:59 +0800

    第PPython第二章字符串完成50%%

---
+++ simple_message.py
@@ -0,0 +1,56 @@
+message = "Hello Lihailing!"
+print(message)
+
+
+
+
+message = "ada lovelace"
+
+
+name = "Abf"
+print(name.title())
+print(name.lower())
+first_name = "ada"
+last_name = "lovelance"
+full_name = first_name+" "+last_name
+print(full_name)
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name+" "+last_name
+print("Hello, "+full_name.title()+"!")
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name+" "+last_name
+message = "Hello, "+full_name.title()+"!"
+print(message) 
+print("Python")
+print("\tpython")
+print("Languages:\nPython\nC\nJavaScript")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
