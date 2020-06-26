3028a2e7e65c196f3f3c64a00c9352a1faaf8894
Zeal706 <1615284247@qq.com>
2020-06-26 23:00:44 +0800

    Four part

---
+++ Four part.py
@@ -0,0 +1,103 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title()+", that was a great trick!")
+    print("I can't wait to see your next trick,"+magician.title()+".\n")
+print("Thank you, everyone. That was a great magic show!")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+message = "Hello Lihailing!"
+print(message)
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title()+", that was a great trick!")
+    print("I can't wait to see your next trick,"+magician.title()+".\n")
+    print("Thank you, everyone. That was a great magic show!")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+for value in range(1,5):
+    print(value)
+for value in range(1,6):
+    print(value)
+numbers = list(range(1,6))
+print(numbers)
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+even_numbers = list(range(3,12,3))
+print(even_numbers)
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
+squares = []
+for value in range(1,11):
+    squares.append(value**2)
+print(squares)
+digits = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+squares = [value**2 for value in range(1,11)]
+print(squares)
+numbers = [value**3 for value in range(1,14)]
+print(numbers)
+for value in range(1,21):
+    print(value)
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[1:4])
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+pplayers = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[2:])
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[-3:])
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print("Here are the first three players on my team:")
+for player in players[:3]:
+    print(player.title())
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+my_foods = ['pizza', 'falafel', 'carrot cake']
+#这行不通
+friend_foods = my_foods
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
+dimensions = (200, 50)
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
