47bf0ca6dc9708c5089133d73aff7c7c64fb7037
MaYatao <mytao1997@163.com>
2020-07-13 13:46:50 +0800

    我又来捣蛋啦

--- apostrophe.py
+++ apostrophe.py
@@ -1,5 +1,5 @@
 message = "one of Python's strengths is its diverse community"
 print(message)
-message =  'one of Python's strengths is its diverse community.'
+message =  "one of Python's strengths is its diverse community."
 print(message)
 
